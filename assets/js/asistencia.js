af74bcf49e0d54f1ecb159f8e062a6c42dc2de1f
ditteruu <droquetercero@gmail.com>
2025-04-21 22:24:41 -0600

    :sparkles: navbar add

---
+++ assets/js/asistencia.js
@@ -0,0 +1,6 @@
+flatpickr("#fecha-header", {
+    dateFormat: "j \\de F \\de Y", // Formato de fecha
+    locale: "es",                  // Localización en español
+    defaultDate: "today"           // Fecha por defecto: hoy
+  });
+  
